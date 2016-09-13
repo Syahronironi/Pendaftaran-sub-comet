b5a21576b46edf5a8de4ce130a45e3137394a097
ASR <karepmuron@gmail.com>
2016-09-13 13:01:48 +0700

    Tahap penambahan komponen-komponen pada layout

---
+++ app/src/main/java/id/sch/tugas01smktelkom_mlg/xiirpl1003/pendaftaransubcomet/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.tugas01smktelkom_mlg.xiirpl1003.pendaftaransubcomet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
